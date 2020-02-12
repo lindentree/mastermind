6b459e677512dce2b30e4ebba3ed57866402d219
Linden Chiu <hchiu87@gmail.com>
2020-02-11 22:05:41 -0800

    set up prompt

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const prompt = require('prompt');
+
+prompt.start();
+ 
+  
+prompt.get(['username'], function (err, result) {
+    
+  console.log('Command-line input received:');
+  console.log('  username: ' + result.username);
+    
+});
