1b4bd4fbf82674bc98aa4da7e7fa4d4049834a3f
Linden Chiu <hchiu87@gmail.com>
2020-02-13 00:14:34 -0800

    stub puzzle function, update readme

---
+++ README.md
@@ -0,0 +1,11 @@
+This is an implementation of the code game Mastermind with a command-line interface, built in Node
+
+# Setup
+
+This project requires Node.js to run. (https://nodejs.org/en/download/).
+
+Steps:
+
+1. git clone this repo
+2. run `npm install` in your terminal
+3. then run `npm run start` or `node index.js`
--- puzzle_completion.js
+++ puzzle_completion.js
@@ -1,3 +1,31 @@
 //check guesses against puzzle
 //output feedback, eg correct numbers, correct numbers and locations, completely wrong etc.
 const utils = require('./utils');
+
+const feedbackOptions = {
+
+  1: `You got ${x} of the numbers correct!`,
+  2: `You got ${location} of the numbers in the right spot!`,
+  3: `All your guesses were wrong.`
+
+}
+
+function puzzleCheck(puzzle, guess) {
+
+  this.feedback = {};
+  this.puzzle = puzzle;
+  let x = 1;
+  let location = 0;
+  let output = '';
+
+  this.checkGuess = function() {
+    for (let i = 0; i < puzzle.length; i += 1) {
+
+    } 
+  }
+
+  this.displayFeedback = function() {
+    
+  }
+
+}
